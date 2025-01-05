1178eff0bbbb3c3f66bea830f6f77b743d6ab985
James Barret <jamesbarret715@gmail.com>
2025-01-05 14:46:13 +0000

    implement config rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+    rewrites: async () => [
+        {
+            source: "/",
+            destination: "/submit"
+        },
+        {
+            source: "/admin",
+            destination: "/admin/home"
+        }
+    ]
 };
 
 export default nextConfig;
